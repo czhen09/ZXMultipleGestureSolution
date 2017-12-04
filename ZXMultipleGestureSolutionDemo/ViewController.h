a7be788d7aadd6f07a09a160e3d353837469898a
ZhengXu <zhengxu@zz.local>
2017-12-04 16:45:05 +0800

    Initial Commit

---
+++ ZXMultipleGestureSolutionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZXMultipleGestureSolutionDemo
+//
+//  Created by 郑旭 on 2017/12/4.
+//  Copyright © 2017年 郑旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZXMultipleGestureSolutionDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZXMultipleGestureSolutionDemo
+//
+//  Created by 郑旭 on 2017/12/4.
+//  Copyright © 2017年 郑旭. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
